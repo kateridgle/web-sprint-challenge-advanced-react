c1a0ab6151180da800037028eff244c343cb39c6
Kate Ridgle <kroo.2394@gmail.com>
2023-07-22 13:19:57 -0700

    changed move and getXY and some jsx but no change

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -13,31 +13,28 @@ const initialState = {
 export default function AppFunctional(props) {
 
   const [state, setState] = useState(initialState);
-  const [email, setEmail] = useState(initialEmail)
-  const [message, setMessage] = useState(initialMessage)
-  const [index, setIndex] = useState(initialIndex)
-  const [steps, setSteps] = useState(initialSteps)
-  // const [x, setX] = useState() unsure if need actual coordinates if index is used
-  // const { message, email, steps, index, x, y } = state; // commented out because seems to conflict 
+  // const [email, setEmail] = useState(initialEmail)
+  // const [message, setMessage] = useState(initialMessage)
+  // const [index, setIndex] = useState(initialIndex)
+  // const [steps, setSteps] = useState(initialSteps)
+  // unsure if need actual coordinates if index is used
+  // const [x, setX] = useState() 
+  const { message, email, steps, index, x, y } = state; // commented out because seems to conflict 
 
-  function getXY() {
-    // It it not necessary to have a state to track the coordinates.
-    // It's enough to know what index the "B" is at, to be able to calculate them.
-    const x = (state.index % 3) + 1
-
-    let y
-    if (state.index < 3) {
-      y = 1
+  const getXY = (index) => {
+    const x = (index % 3) + 1;
+  
+    let y;
+    if (index < 3) {
+      y = 1;
+    } else if (index < 6) {
+      y = 2;
+    } else if (index < 9) {
+      y = 3;
     }
-    else if (state.index < 6) {
-      y = 2
-    }
-    else if (state.index < 9) {
-      y = 3
-    }
-
+  
     return [x, y];
-  }
+  };
 
   function getXYMessage() {
     // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
@@ -66,70 +63,70 @@ export default function AppFunctional(props) {
     // This event handler can use the helper above to obtain a new index for the "B",
     // and change any states accordingly.
     if (evt.target.id === "up") {
-      // console.log(evt); it does register click
-
       if (state.index > 2) {
-        setState({
-          ...state,
-          index: state.index - 3,
+        setState((prevState) => ({
+          ...prevState,
+          index: prevState.index - 3,
           message: "",
-          steps: state.steps + 1,
-          y: state.y - 1
-        })
+          steps: prevState.steps + 1,
+          y: prevState.y - 1
+        }));
       } else {
-        setState({ ...state, message: "You can't go up" })
+        setState((prevState) => ({ ...prevState, message: "You can't go up" }));
       }
-
     }
     if (evt.target.id === "down") {
-      // console.log(evt);
-
       if (state.index < 6) {
-        setState({
-          ...state,
-          index: state.index + 3,
+        setState((prevState) => ({
+          ...prevState,
+          index: prevState.index + 3,
           message: "",
-          steps: state.steps + 1,
-          y: state.y + 1
-        })
+          steps: prevState.steps + 1,
+          y: prevState.y + 1
+        }));
       } else {
-        setState({ ...state, message: "You can't go down" })
+        setState((prevState) => ({ ...prevState, message: "You can't go down" }));
       }
-
     }
     if (evt.target.id === "right") {
-      // console.log(evt)
-      if (state.index === 1 || state.index === 4 || state.index === 7 || state.index === 0 || state.index === 3 ||
-        state.index === 6) {
-        setState({
-          ...state,
-          index: state.index + 1,
+      if (
+        state.index === 1 ||
+        state.index === 4 ||
+        state.index === 7 ||
+        state.index === 0 ||
+        state.index === 3 ||
+        state.index === 6
+      ) {
+        setState((prevState) => ({
+          ...prevState,
+          index: prevState.index + 1,
           message: "",
-          steps: state.steps + 1,
-          x: state.x + 1
-        })
-
+          steps: prevState.steps + 1,
+          x: prevState.x + 1
+        }));
       } else {
-        setState({ ...state, message: "You can't go right" })
+        setState((prevState) => ({ ...prevState, message: "You can't go right" }));
       }
-
     }
-
     if (evt.target.id === "left") {
-      // console.log(evt)
-      if (state.index === 1 || state.index === 4 || state.index === 7 || state.index === 2 || state.index === 5 ||
-        state.index === 8) {
-        setState({
-          ...state,
-          index: state.index - 1,
+      if (
+        state.index === 1 ||
+        state.index === 4 ||
+        state.index === 7 ||
+        state.index === 2 ||
+        state.index === 5 ||
+        state.index === 8
+      ) {
+        setState((prevState) => ({
+          ...prevState,
+          index: prevState.index - 1,
           message: "",
-          steps: state.steps + 1,
-          x: state.x - 1
-        })
+          steps: prevState.steps + 1,
+          x: prevState.x - 1
+        }));
       } else {
-        setState({ ...state, message: "You can't go left" })
+        setState((prevState) => ({ ...prevState, message: "You can't go left" }));
       }
-
     }
   }
 
@@ -165,11 +162,11 @@ export default function AppFunctional(props) {
         <h3 value={message} id="message">{props.message}</h3>
       </div>
       <div id="keypad">
-        <button value={index} onClick={move} id="left">LEFT</button>
-        <button value={index} onClick={move} id="up">UP</button>
-        <button value={index} onClick={move} id="right">RIGHT</button>
-        <button value={index} onClick={move} id="down">DOWN</button>
-        <button value={index} onClick={move} id="reset">reset</button>
+        <button value={index} onClick={state.move} id="left">LEFT</button>
+        <button value={index} onClick={state.move} id="up">UP</button>
+        <button value={index} onClick={state.move} id="right">RIGHT</button>
+        <button value={index} onClick={state.move} id="down">DOWN</button>
+        <button value={index} onClick={state.move} id="reset">reset</button>
       </div>
       <form onSubmit={onSubmit}>
         <input value={email} onChange={onChange} id="email" type="email" placeholder="type email"></input>
