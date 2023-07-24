2d11c7d80d8d080ca43621253895e16c7ba5b17e
Kate Ridgle <kroo.2394@gmail.com>
2023-07-24 12:18:08 -0700

    problems with a 422 error and coordinates showing up

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -167,7 +167,7 @@ export default function AppFunctional(props) {
     <div id="wrapper" className={props.className}>
       <div className="info">
         <h3 id="coordinates">{getXYMessage(index)}</h3>
-        <h3 id="steps">You moved {steps} time {steps !== 1 ? 's' : ''}</h3>
+        <h3 id="steps">You moved {steps} time{steps !== 1 ? 's' : ''}</h3>
       </div>
       <div id="grid">
         {
